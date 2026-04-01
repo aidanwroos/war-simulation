4d36ca3a97e578d29fba3f716c0dd86ce3dc9358
Aidan Roos <aidanwroos@gmail.com>
2026-03-31 18:31:59 -0700

    add simple linkedlist implementation

--- basics/Program.cs
+++ basics/Program.cs
@@ -3,28 +3,73 @@ using System;
 
 class Program
 {
-
-    public class Person
+    //generic Node class using <T>
+    public class Node<T>
     {
-        public int age;
-        public string name;
+        public T data;
+        public Node<T>? Next;
 
-        public Person(int age, string name)
+        //Node constructor
+        public Node(T data)
         {
-            this.age = age;
-            this.name = name;
+            this.data = data;
+            this.Next = null;
         }
     }
-   
-    
 
-
-
-
-    static void Main(string[] args)
+    //generic LinkedList class can be of any type Node<T>
+    public class LinkedList<T>
     {
-        Person person1 = new Person(23, "Aidan");
-        Console.WriteLine("Name is: " + person1.name + "\nAge is: " + person1.age);
+        //'?' means the variable is allowed to be null
+        private Node<T>? head;
+
+        public void AppendLast(T data)
+        {
+            //creates a new Node<T> internally
+            var new_node = new Node<T>(data);
+
+            //head is null (list is empty)
+            if (head == null)
+            {
+                head = new_node;
+                return;
+            }
+
+            //head isn't null, (list has nodes already)
+            //traverse list to last node, set new node to be last
+            var current = head;
+            while (current.Next != null)
+            {
+                current = current.Next;
+            }
+            current.Next = new_node;
+        }
+
+        public void PrintList()
+        {
+            var current = head;
+            while (current != null)
+            {
+                Console.Write(current.data + "->");
+                current = current.Next;
+            }
+            //last node is null
+            Console.WriteLine("null");
+        }
+
+    }
+
+    static void Main()
+    {
+        var ll = new LinkedList<int>(); //linkedlist object of type 'int'
+        
+        for (int i = 0; i < 20; i++)
+        {
+            ll.AppendLast(i);
+        }
+
+        ll.PrintList();
+
     }
 }
 
